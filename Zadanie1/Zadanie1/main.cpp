5efe0a65ea3a3d21d8a9e4c1155a196bd6a88b73
Mykhailo Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2023-11-18 18:01:55 +0100

    update

--- Zadanie1/Zadanie1/main.cpp
+++ Zadanie1/Zadanie1/main.cpp
@@ -18,10 +18,6 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	if (strcmp(argv[2], "debug")) {
-#define DEBUG 1
-	}
-
 	char portName[6];
 	HANDLE comPort;
 	const int bufferSize = 32;
@@ -35,9 +31,7 @@ int main(int argc, char* argv[]) {
 		return 0;
 	Sleep(200);
 
-#ifdef DEBUG
 	printf("%s\n", argv[1]);
-#endif // DEBUG
 
 	if (strcmp(argv[1], "rx") == 0) {
 		printf("Enter you pattern(use _ to represent any letter) and lenght: ");
@@ -46,17 +40,13 @@ int main(int argc, char* argv[]) {
 		uint8_t wordLength;
 		uint32_t bytes;
 		if (scanf_s("%31s %hhu", word, sizeof(word), &wordLength) == 2) {
-#ifdef DEBUG
 			printf("%s + %d\n", word, wordLength);
 			printf("Word successful entered\n");
-#endif // DEBUG
 
 			buffer[0] = (char*)malloc(strlen(word) + 1);
 			buffer[1] = (char*)malloc(2);
 			if (buffer[0] == NULL || buffer[1] == NULL) {
-#ifdef DEBUG
 				printf("Memory allocation failed\n");
-#endif // DEBUG
 				return 13;
 			}
 
@@ -65,10 +55,8 @@ int main(int argc, char* argv[]) {
 			buffer[1][0] = (char)wordLength;
 			buffer[1][1] = '\0';
 
-#ifdef DEBUG
 			printf("Word in buffer: %s\n", buffer[0]);
 			printf("Lenght word in buffer: %d\n", buffer[1][0]);
-#endif // DEBUG
 
 			if (writePort(&comPort, buffer, bufferSize, &bytes, false) == 0) {
 				printf("Error writing to serial port\n");
@@ -80,11 +68,9 @@ int main(int argc, char* argv[]) {
 				printf("Success writing to serial port\n");
 			}
 
-#ifdef DEBUG
 			printf("END FUNCTION buffer[0] = %s\n", buffer[0]);
 			printf("END FUNCTION buffer[1] = %d\n", buffer[1][0]);
 			printf("END FUNCTION bytes = %d\n", bytes);
-#endif // DEBUG
 
 			free(buffer[0]);
 			free(buffer[1]);
@@ -109,62 +95,65 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	else if (strcmp(argv[1], "tx") == 0) {
-		char* readData[2];
-		readData[0] = (char*)malloc(bufferSize);
-		readData[1] = (char*)malloc(2);
-		uint32_t bytes;
-		if (readPort(&comPort, readData, bufferSize, &bytes, false) == 0) {
-			free(readData[0]);
-			free(readData[1]);
-			printf("Error reading from serial port\n");
-			return 11;
-		}
-		else {
-			printf("Success reading from serial port\n");
-		}
+		while (true) {
 
-#ifdef DEBUG
-		printf("END FUNCTION readData[0] = %s\n", readData[0]);
-		printf("END FUNCTION readData[1] = %d\n", readData[1][0]);
-		printf("END FUNCTION bytes = %d\n", bytes);
-#endif // DEBUG
 
-		free(readData[1]);
-
-		// Second part
-		Sleep(1000);
-		char* sendData[1];
-		sendData[0] = (char*)malloc(bytes + 1);
-
-		for (int i = 0; i < strlen(readData[0]); i++) {
-			if (readData[0][i] >= 'a' && readData[0][i] <= 'z') {
-				sendData[0][i] = readData[0][i] - 32;
-			}
-			else if (readData[0][i] >= 'A' && readData[0][i] <= 'Z') {
-				sendData[0][i] = readData[0][i] + 32;
+			char* readData[2];
+			readData[0] = (char*)malloc(bufferSize);
+			readData[1] = (char*)malloc(2);
+			uint32_t bytes;
+			if (readPort(&comPort, readData, bufferSize, &bytes, false) == 0) {
+				free(readData[0]);
+				free(readData[1]);
+				printf("Error reading from serial port\n");
+				return 11;
 			}
 			else {
-				sendData[0][i] = readData[0][i];
+				printf("Success reading from serial port\n");
 			}
 
-		}
-		sendData[0][strlen(readData[0])] = '\0';
-		printf("SENDDATA = %s\n", sendData[0]);
+			printf("END FUNCTION readData[0] = %s\n", readData[0]);
+			printf("END FUNCTION readData[1] = %d\n", readData[1][0]);
+			printf("END FUNCTION bytes = %d\n", bytes);
 
-		uint32_t bytesSend;
-		if (writePort(&comPort, sendData, bytes, &bytesSend, true) == 0) {
+			free(readData[1]);
+
+			// Second part
+			Sleep(500);
+			char* sendData[1];
+			sendData[0] = (char*)malloc(bytes + 1);
+
+			for (int i = 0; i < strlen(readData[0]); i++) {
+				if (readData[0][i] >= 'a' && readData[0][i] <= 'z') {
+					sendData[0][i] = readData[0][i] - 32;
+				}
+				else if (readData[0][i] >= 'A' && readData[0][i] <= 'Z') {
+					sendData[0][i] = readData[0][i] + 32;
+				}
+				else {
+					sendData[0][i] = readData[0][i];
+				}
+
+			}
+			sendData[0][strlen(readData[0])] = '\0';
+			printf("SENDDATA = %s\n", sendData[0]);
+
+			uint32_t bytesSend;
+			if (writePort(&comPort, sendData, bytes, &bytesSend, true) == 0) {
+				free(readData[0]);
+				free(sendData[0]);
+				printf("Error writing to serial port\n");
+				return 12;
+			}
+			else {
+				printf("Success writing to serial port\n", bytesSend);
+			}
 			free(readData[0]);
 			free(sendData[0]);
-			printf("Error writing to serial port\n");
-			return 12;
-		}
-		else {
-			printf("Success writing to serial port\nBytes send: %d\n", bytesSend);
-		}
-		free(readData[0]);
-		free(sendData[0]);
 
 
+
+		}
 	}
 	else {
 		printf("Please reopen file and set argument to 'rx/tx'\n");

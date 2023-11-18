0af1a2b41008e7f5f5844a90f0a485b038e5aef5
Mykhailo Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2023-11-18 17:46:13 +0100

    update

---
+++ Zadanie1/Zadanie1/rs485.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <Windows.h>
+#include <string.h>
+#include <conio.h>
+#include <thread>
+#include <cassert>
+#include <iostream>
+
+bool createPort(HANDLE* comPort, char* portName);
+
+bool initPort(HANDLE* comPort, char* portName, uint32_t ComRate, int ComBits, COMMTIMEOUTS timeout);
+
+bool purgePort(HANDLE* comPort);
+
+bool readPort(HANDLE* comPort, char* buffer[], uint32_t bufferSize, uint32_t* bytesRead, bool receive);
+
+bool writePort(HANDLE* comPort, char* buffer[], uint32_t bufferSize, uint32_t* bytesWritten, bool receive);
+
+bool setComParameters(HANDLE* comPort, uint32_t comRate, int comBits, COMMTIMEOUTS timeout);
